90694af6bfb38b2278a6bf68d975841510b546ca
邢世 <xingshi@heatedloan.com>
2018-01-23 15:25:15 +0800

    init

---
+++ frms-test/src/main/java/com/nuanshui/frms/test/mapper/example/ExampleMapper.java
@@ -0,0 +1,12 @@
+package com.nuanshui.frms.test.mapper.example;
+
+import com.nuanshui.frms.test.entity.Example;
+
+/**
+ * Created by Administrator on 2017/2/28.
+ */
+public interface ExampleMapper {
+
+    Long insert(Example example);
+
+}
